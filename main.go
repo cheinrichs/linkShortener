ca654a830d22cf5a8829d84732ef2ba2cfd0c67f
Cooper Heinrichs <cooper.heinrichs@gmail.com>
2020-02-08 16:26:58 -0700

    removed route setup that was moved to routes.go

--- main.go
+++ main.go
@@ -1,33 +1,21 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
-	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 )
 
-var dbURL string
 var port string
-var host string
-
-var envVariableOk bool
 var defaultPort = "8080"
-
-var db *sql.DB
-var dbErr error
+var host string
+var envVariableOk bool
 
 //initializeEnv sets up all environment variables and prints warnings if something is missing
 func initializeEnv() {
-	dbURL, envVariableOk = os.LookupEnv("DATABASE_URL")
-	if !envVariableOk {
-		fmt.Println("DATABASE_URL not set.")
-	}
-
 	port, envVariableOk = os.LookupEnv("PORT")
 	if !envVariableOk {
 		fmt.Println("PORT not set.")
@@ -44,11 +32,7 @@ func main() {
 
 	initializeEnv()
 
-	router := mux.NewRouter()
-
-	router.HandleFunc("/createLink", createLinkEndpoint).Methods("POST")
-	router.HandleFunc("/linkStatistics/{redirectHash}", linkStatisticsEndpoint).Methods("GET")
-	router.HandleFunc("/{redirectHash}", redirectEndpoint).Methods("GET")
+	router := NewRouter()
 
 	if !(port == "") {
 		fmt.Println("atmzr started and listening on port " + port)
