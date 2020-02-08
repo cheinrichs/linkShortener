33d0c3991fffe732879ff98c667d5094e65e00be
Cooper Heinrichs <cooper.heinrichs@gmail.com>
2020-02-08 16:27:45 -0700

    added logging to all routes with a wrapper function

--- router.go
+++ router.go
@@ -1,17 +1,28 @@
 package main
 
-import "github.com/gorilla/mux"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 //NewRouter sets up all the routes for the mux Router
 func NewRouter() *mux.Router {
 
 	router := mux.NewRouter().StrictSlash(true)
+
 	for _, route := range routes {
+
+		var handler http.Handler
+
+		handler = route.HandlerFunc
+		handler = Logger(handler, route.Name)
+
 		router.
 			Methods(route.Method).
 			Path(route.Pattern).
 			Name(route.Name).
-			Handler(route.HandlerFunc)
+			Handler(handler)
 	}
 
 	return router
