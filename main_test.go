e4617acd0edc89cc66d4fe432ca35d6c7c9c57bd
Cooper Heinrichs <cooper.heinrichs@gmail.com>
2020-02-09 22:38:45 -0700

    added a valid url redirect test

--- main_test.go
+++ main_test.go
@@ -21,7 +21,7 @@ type Mockdb struct {
 
 //FindRedirectURLByID mocked for testing
 func (m Mockdb) FindRedirectURLByID(linkID byte) (string, error) {
-	return "", nil
+	return "http://www.google.com", nil
 }
 
 //RecordView mocked for testing
@@ -52,6 +52,15 @@ func init() {
 //CreateLinkEndpoint
 
 //RedirectEndpoint
+func TestRedirectEndpointValidRedirect(t *testing.T) {
+
+	request, _ := http.NewRequest("GET", "/QQ==", nil)
+	response := httptest.NewRecorder()
+
+	Router().ServeHTTP(response, request)
+
+	assert.Equal(t, "http://www.google.com", response.Header()["Location"][0], "Redirecting to incorrect url.")
+}
 
 //IndexEndpoint
 func TestIndexEndpoint(t *testing.T) {
@@ -63,7 +72,7 @@ func TestIndexEndpoint(t *testing.T) {
 
 	b, _ := ioutil.ReadAll(response.Body)
 	expected := "<!DOCTYPE html>\n<html>\n\t<head>\n\t\t<meta charset=\"utf-8\">\n\t\t<title>atmzr</title>\n\t\t<style>\n\t\t\tbody {\n\t\t\t\tfont-family: sans-serif;\n\t\t\t}\n\t\t</style>\n\t</head>\n\t<body>\n\t\t<h1>atmzr</h1>\n        <p>Welcome to atmzr!</p>\n        <p>Please visit <a href=\"https://github.com/cheinrichs/linkShortener\">https://github.com/cheinrichs/linkShortener</a> for more information.</p>\n\t</body>\n</html>"
-	assert.Equal(t, expected, string(b), "Existing link returns correct data.")
+	assert.Equal(t, expected, string(b), "Index does not return the correct HTML.")
 }
 
 //LinkStatisticsEndpoint
@@ -77,7 +86,7 @@ func TestLinkStatisticsEndpointLinkExistsWithData(t *testing.T) {
 
 	b, _ := ioutil.ReadAll(response.Body)
 
-	assert.Equal(t, "{\"status\":\"success\",\"data\":\"1\"}\n", string(b), "Existing link returns correct data.")
+	assert.Equal(t, "{\"status\":\"success\",\"data\":\"1\"}\n", string(b), "Does not find the correct data.")
 }
 
 //Test link does not exist
@@ -88,7 +97,7 @@ func TestLinkStatisticsEndpointLinkDoesNotExist(t *testing.T) {
 
 	b, _ := ioutil.ReadAll(response.Body)
 
-	assert.Equal(t, "{\"status\":\"success\",\"data\":\"0\"}\n", string(b), "Existing link returns correct data.")
+	assert.Equal(t, "{\"status\":\"success\",\"data\":\"0\"}\n", string(b), "Fails to find 0 views for a link that does not exist.")
 }
 
 //Test empty redirect hash
@@ -99,7 +108,7 @@ func TestLinkStatisticsEndpointEmptyRedirectHash(t *testing.T) {
 
 	b, _ := ioutil.ReadAll(response.Body)
 
-	assert.Equal(t, "{\"status\":\"error\",\"data\":\"Please include a hash.\"}\n", string(b), "Existing link returns correct data.")
+	assert.Equal(t, "{\"status\":\"error\",\"data\":\"Please include a hash.\"}\n", string(b), "Fails to alert the user to include a hash.")
 }
 
 //Test redirect hash that's too small
@@ -109,7 +118,7 @@ func TestLinkStatisticsEndpointRedirectHashTooSmall(t *testing.T) {
 	Router().ServeHTTP(response, request)
 
 	b, _ := ioutil.ReadAll(response.Body)
-	assert.Equal(t, "{\"status\":\"error\",\"data\":\"Please provide a valid hash.\"}\n", string(b), "Existing link returns correct data.")
+	assert.Equal(t, "{\"status\":\"error\",\"data\":\"Please provide a valid hash.\"}\n", string(b), "Fails to alert the user to include a hash.")
 }
 
 //TestDecodeID makes sure we decode numbers correctly
@@ -121,7 +130,7 @@ func TestDecodeID(t *testing.T) {
 		t.Errorf("Error: %s", err.Error())
 	}
 
-	assert.Equal(t, expected, result, "Expecting `73`")
+	assert.Equal(t, expected, result, "Decoding working incorrectly")
 }
 
 //TestEncodeID makes sure we encode numbers correctly
@@ -130,5 +139,5 @@ func TestEncodeID(t *testing.T) {
 	var expected = "SQ=="
 	result := EncodeID(input)
 
-	assert.Equal(t, expected, result, "Expecting `SQ==`")
+	assert.Equal(t, expected, result, "Encoding working incorrectly")
 }
